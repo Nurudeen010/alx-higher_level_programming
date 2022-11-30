d5b935b6d66bc365960ba01c914f8cd4446cfabe
Nurudeen010 <nurudeenolamide010@gmail.com>
2022-11-30 13:08:09 -0800

    Another one

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(3*str)
+kunle = str[0:10]
+print(kunle)
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print('''The Zen of Python”, by TimPeters
+    Beautiful is better than ugly.''')
